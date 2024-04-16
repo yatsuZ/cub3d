c9ae05aa5f5fe0a720a209a5ae7dc1e528f0eb6e
Lara Zanett <larazanetti207@gmail.com>
2024-04-16 13:10:45 +0200

    image change

--- code/v0/Source/executing/move.c
+++ code/v0/Source/executing/move.c
@@ -6,7 +6,7 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 11:53:50 by lazanett          #+#    #+#             */
-/*   Updated: 2024/04/16 12:17:18 by lazanett         ###   ########.fr       */
+/*   Updated: 2024/04/16 12:23:55 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ void	go_straight(t_all_data *all)
 	if (all->file->mapp[(int)(all->file->campos.y + all->file->raydir.y \
 	* 0.045)][(int)all->file->campos.x] != '1')
 		all->file->campos.y += all->file->raydir.y * 0.045;
-	if (all->file->mapp[(int)all->file->campos.y]\
+	if (all->file->mapp[(int)all->file->campos.y] \
 	[(int)(all->file->campos.x + all->file->raydir.x \
 		* 0.045)] != '1')
 		all->file->campos.x += all->file->raydir.x * 0.045;
 	raycasting(all);
-
 }
 
 void	go_back(t_all_data *all)
@@ -30,7 +29,7 @@ void	go_back(t_all_data *all)
 	if (all->file->mapp[(int)(all->file->campos.y - all->file->raydir.y \
 	* 0.045)][(int)all->file->campos.x] != '1')
 		all->file->campos.y -= all->file->raydir.y * 0.045;
-	if (all->file->mapp[(int)all->file->campos.y]\
+	if (all->file->mapp[(int)all->file->campos.y] \
 	[(int)(all->file->campos.x - all->file->raydir.x \
 	* 0.045)] != '1')
 		all->file->campos.x -= all->file->raydir.x * 0.045;
