d4aefc0d2624d356e8607d1dafe30af036ce2ff9
Lara Zanett <larazanetti207@gmail.com>
2024-04-19 13:32:06 +0200

    minimap add

--- code/v0/Source_bonus/executing_bonus/minimap.c
+++ code/v0/Source_bonus/executing_bonus/minimap.c
@@ -6,7 +6,7 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:51:53 by lazanett          #+#    #+#             */
-/*   Updated: 2024/04/17 18:08:04 by lazanett         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:27:58 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	draw_minimap(t_all_data *all, int y, int x)
 	char	cell;
 
 	init_minimap(all);
+	
 	current_y_bis = all->file->bis.y;
 	y = all->file->start.y;
 	while (y < all->file->end.y)
@@ -26,20 +27,89 @@ void	draw_minimap(t_all_data *all, int y, int x)
 		x = all->file->start.x;
 		while (x < all->file->end.x)
 		{
+			
 			cell = all->file->mapp[y][x];
+			// draw_square(all, (all->file->campos.y - (float)all->file->start.y) * 5.0, \
+			// (all->file->campos.x  - (float)all->file->start.x) * 5.0 , 0xF11818);
+			
 			if (cell == '1')
 				draw_square(all, current_y_bis, all->file->bis.x, 0xACAAA8);
+			else if (y == (int)all->file->campos.y && x == (int)all->file->campos.x)
+				draw_square(all, current_y_bis, all->file->bis.x, 0xF11818);
+			//draw_cam_minimap(all);
 			else if (cell == '0' || is_dir(cell))
 				draw_square(all, current_y_bis, all->file->bis.x, 0xFFF7F7);
+			else if (cell == '2')
+				draw_square(all, current_y_bis, all->file->bis.x, 0xFFE4B5);
 			x++;
 			all->file->bis.x += 5;
 		}
 		current_y_bis += 5;
 		y++;
 	}
-	draw_cam_minimap(all);
+
 }
 
+// static void draw_minimap2(t_all_data *all, int y, int x)
+// {
+// 	int		current_y_bis;
+// 	char	cell;
+
+// 	draw_cam_minimap(all);
+// 	current_y_bis = all->file->bis.y;
+// 	y = all->file->start.y;
+// 	printf("y = %d | x = %d | all->file->end.y = %f | all->file->end.x = %f\n", y, x, all->file->end.y, all->file->end.x);
+// 	while (y < all->file->end.y)
+// 	{
+// 		printf("hellooo\n");
+// 		all->file->bis.x = 0;
+// 		x = all->file->start.x;
+// 		while (x < all->file->end.x)
+// 		{
+// 			cell = all->file->mapp[y][x];
+// 			printf("hello\n");
+// 			if (cell == '1')
+// 				draw_square(all, current_y_bis, all->file->bis.x, 0xACAAA8);
+// 			else if (cell == '2')
+// 				draw_square(all, current_y_bis, all->file->bis.x, 0xFFE4B5);
+// 			x++;
+// 			all->file->bis.x += 5;
+// 		}
+// 		current_y_bis += 5;
+// 		y++;
+// 	}
+// }
+
+// void	draw_minimap(t_all_data *all, int y, int x)
+// {
+// 	int		current_y_bis;
+// 	char	cell;
+
+// 	current_y_bis = all->file->bis.y;
+// 	y = all->file->start.y;
+// 	printf("y = %d | x = %d | all->file->end.y = %f | all->file->end.x = %f\n", y, x, all->file->end.y, all->file->end.x);
+// 	while (y < all->file->end.y)
+// 	{
+// 		all->file->bis.x = 0;
+// 		x = all->file->start.x;
+// 		while (x < all->file->end.x)
+// 		{
+			
+// 			cell = all->file->mapp[y][x];
+// 			// if (cell == '1')
+// 			// 	draw_square(all, current_y_bis, all->file->bis.x, 0xACAAA8);
+// 			if (cell == '0' || is_dir(cell))
+// 				draw_square(all, current_y_bis, all->file->bis.x, 0xFFF7F7);
+// 			// else if (cell == '2')
+// 			// 	draw_square(all, current_y_bis, all->file->bis.x, 0xFFE4B5);
+// 			x++;
+// 			all->file->bis.x += 5;
+// 		}
+// 		current_y_bis += 5;
+// 		y++;
+// 	}
+// 	draw_minimap2(all, -1, -1);
+// }
 void	draw_square(t_all_data *all, int y, int x, int color)
 {
 	int	y_temp;
--- code/v0/Source_bonus/executing_bonus/minimap_utils.c
+++ code/v0/Source_bonus/executing_bonus/minimap_utils.c
@@ -6,7 +6,7 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 17:59:19 by lazanett          #+#    #+#             */
-/*   Updated: 2024/04/17 18:09:01 by lazanett         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:26:04 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,13 @@ void	init_minimap(t_all_data *all)
 
 void	draw_cam_minimap(t_all_data *all)
 {
-	all->file->player_draw.x = (all->file->campos.x - all->file->start.x) * 5;
-	all->file->player_draw.y = (all->file->campos.y - all->file->start.y) * 5;
-	all->file->player_draw.x = max(0, min(all->file->player_draw.x, \
-	all->file->perimeter_size * 5));
-	all->file->player_draw.y = max(0, min(all->file->player_draw.y, \
-	all->file->perimeter_size * 5));
-	draw_square(all, all->file->player_draw.y - 2.02, \
-	all->file->player_draw.x - 0.20, 0xF11818);
+	all->file->player_draw.x = (all->file->campos.x - (float)all->file->start.x) * 5.0;
+	all->file->player_draw.y = (all->file->campos.y - (float)all->file->start.y) * 5.0;
+	// all->file->player_draw.x = max(0, min(all->file->player_draw.x, \
+	// all->file->perimeter_size * 5));
+	// all->file->player_draw.y = max(0, min(all->file->player_draw.y, \
+	// all->file->perimeter_size * 5));
+	draw_square(all, all->file->player_draw.y, \
+	all->file->player_draw.x, 0xF11818);
+
 }
